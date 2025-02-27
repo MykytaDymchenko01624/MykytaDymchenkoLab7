5dd8ef07931802b596f5db48ee1bff2a689e1ea6
Prozarius <prozarius2207@gmail.com>
2025-02-26 23:50:49 -0500

    Changed main icon of the app and added bottom navigation and menu

--- app/src/main/java/mykyta/dymchenko/n01624422/MainActivity.kt
+++ app/src/main/java/mykyta/dymchenko/n01624422/MainActivity.kt
@@ -1,20 +1,33 @@
+// Mykyta Dymchenko N01624422
 package mykyta.dymchenko.n01624422
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
+import android.widget.Toolbar
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        setSupportActionBar(findViewById(R.id.toolBarHelp))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.upper_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.up_settings -> Toast.makeText(this,"Settings Selected",Toast.LENGTH_SHORT).show()
+            R.id.up_help -> Toast.makeText(this,"Help Selected", Toast.LENGTH_SHORT).show()
         }
+        return super.onOptionsItemSelected(item)
     }
 }
